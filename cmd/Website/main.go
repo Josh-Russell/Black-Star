675359e52dc42e9982fc725df50fc3a14179d99f
Joshua Russell <bawbmaggit@gmail.com>
2016-02-18 21:51:13 -0700

    add several pages and the correct routes to those pages.

--- cmd/Website/main.go
+++ cmd/Website/main.go
@@ -23,6 +23,21 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+	router.GET("/view", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "view.tmpl.html", nil)
+	})
+	router.GET("/upload", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "upload.tmpl.html", nil)
+	})
+	router.GET("/search", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "search.tmpl.html", nil)
+	})
+	router.GET("/register", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "register.tmpl.html", nil)
+	})
+	router.GET("/profile", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "profile.tmpl.html", nil)
+	})
 
 	router.Run(":" + port)
 }
